14cbeb7f50b0f3e39112db4cceb81ec323b38499
Hamidreza Alimohammadi <alimohammadi.hamidreza@gmail.com>
2023-06-21 17:14:01 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+curr_ = Path(__file__).parent
+long_description = (curr_ / 'README.md').read_text()
+
+setup(
+    name='dcl2nwb',
+    version='0.0.1',
+    author='Hamidreza Alimohammadi',
+    author_email="<alimohammadi.hamidreza@gmail.com>",
+    description='adaptive and customizable conversion tool to standardize the data storage of Defense Circuits Lab (DCL) into NWB format',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    keywords=['Python', 'NWB', 'ReTune', 'DCL'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux :: Ubuntu",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
